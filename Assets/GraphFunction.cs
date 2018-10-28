3cc70ef8b21b138945defd777e421f74609709e2
Adam Riggs <phlux1111@gmail.com>
2018-10-27 22:34:41 -0400

    first commit

---
+++ Assets/GraphFunction.cs
@@ -0,0 +1,6 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+//public delegate float GraphFunction(float x, float z, float t);
+public delegate Vector3 GraphFunction(float u, float v, float t);
